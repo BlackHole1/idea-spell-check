4d06890efe254f0c692e31988a912e14758b7f46
Pierre Fouilloux <7494211+pfouilloux@users.noreply.github.com>
2025-04-13 18:47:15 +0800

    build: update gradle plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,17 +7,17 @@ plugins {
     // Java support
     id("java")
     // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "1.7.21"
+    id("org.jetbrains.kotlin.jvm") version "2.1.20"
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.10.1"
+    id("org.jetbrains.intellij") version "1.17.4"
     // Gradle Changelog Plugin
-    id("org.jetbrains.changelog") version "2.0.0"
+    id("org.jetbrains.changelog") version "2.2.1"
     // Gradle Qodana Plugin
     id("org.jetbrains.qodana") version "0.1.13"
     // Gradle Kover Plugin
-    id("org.jetbrains.kotlinx.kover") version "0.6.1"
+    id("org.jetbrains.kotlinx.kover") version "0.9.1"
 
-    kotlin("plugin.serialization") version "1.7.21"
+    kotlin("plugin.serialization") version "2.1.20"
 }
 
 dependencies {
@@ -64,8 +64,12 @@ qodana {
 }
 
 // Configure Gradle Kover Plugin - read more: https://github.com/Kotlin/kotlinx-kover#configuration
-kover.xmlReport {
-    onCheck.set(true)
+kover {
+    reports {
+        total {
+            xml.onCheck = true
+        }
+    }
 }
 
 tasks {
