20749ef674d1d9dcb5b6eeec0eb85764c22c5982
Kevin Cui <bh@bugs.cc>
2025-09-07 23:41:55 +0800

    chore: migrate to the latest template
    Signed-off-by: Kevin Cui <bh@bugs.cc>

--- src/main/kotlin/com/github/blackhole1/ideaspellcheck/startup/SCProjectActivity.kt
+++ src/main/kotlin/com/github/blackhole1/ideaspellcheck/startup/SCProjectActivity.kt
@@ -3,10 +3,10 @@ package com.github.blackhole1.ideaspellcheck.startup
 import com.github.blackhole1.ideaspellcheck.services.SCProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.startup.StartupActivity
+import com.intellij.openapi.startup.ProjectActivity
 
-internal class SCStartupActivity : StartupActivity {
-    override fun runActivity(project: Project) {
+internal class SCProjectActivity : ProjectActivity {
+    override suspend fun execute(project: Project) {
         project.service<SCProjectService>()
     }
 }
